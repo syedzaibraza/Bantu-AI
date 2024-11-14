79245221928e98a947de6b0773ef16b24d68ee0b
Syed Zaib Raza <syedzaibraza@gmail.com>
2024-11-14 20:04:25 +0500

    Banner optimised

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -3,13 +3,22 @@ const Banner = () => {
     <div className=" h-[194px] relative">
       <div className="w-full h-[139px] -left-1 top-[54.47px] absolute origin-top-left -rotate-2 bg-[#191919]"></div>
       <div className="p-9 gap-6 w-full right-0 top-0 absolute origin-top-left rotate-2 bg-[#504cff] justify-end items-center inline-flex">
-        <img src="/icons/MStore_id.svg" className="h-20 w-44" />
-        <img src="/icons/Mangcoding.svg" className="h-20 w-44" />
-        <img src="/icons/Google.svg" className="h-20 w-44" />
-        <img src="/icons/MStore_id.svg" className="h-20 w-44" />
-        <img src="/icons/Mangcoding.svg" className="h-20 w-44" />
-        <img src="/icons/Google.svg" className="h-20 w-44" />
-        <img src="/icons/MStore_id.svg" className="h-20 w-44" />
+        {[
+          "MStore_id",
+          "Mangcoding",
+          "Google",
+          "MStore_id",
+          "Mangcoding",
+          "Google",
+          "MStore_id",
+        ].map((icon, index) => (
+          <img
+            key={index}
+            src={`/icons/${icon}.svg`}
+            className="h-20 w-44 flex-shrink-0"
+            alt={`${icon} logo`}
+          />
+        ))}
       </div>
     </div>
   );
