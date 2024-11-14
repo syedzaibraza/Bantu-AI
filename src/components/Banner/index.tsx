8854781b65932d426c98e2aa2d1c8fc96213e69f
Syed Zaib Raza <syedzaibraza@gmail.com>
2024-11-14 19:59:24 +0500

    Banner fixed

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,8 +1,8 @@
 const Banner = () => {
   return (
     <div className=" h-[194px] relative">
-      <div className="w-screen h-[139px] -left-1 top-[54.47px] absolute origin-top-left -rotate-2 bg-[#191919]"></div>
-      <div className="pl-[27.58px] pt-[35.75px] pb-[33.62px] gap-6 -right-4 top-0 absolute origin-top-left rotate-2 bg-[#504cff] justify-end items-center inline-flex">
+      <div className="w-full h-[139px] -left-1 top-[54.47px] absolute origin-top-left -rotate-2 bg-[#191919]"></div>
+      <div className="p-9 gap-6 w-full right-0 top-0 absolute origin-top-left rotate-2 bg-[#504cff] justify-end items-center inline-flex">
         <img src="/icons/MStore_id.svg" className="h-20 w-44" />
         <img src="/icons/Mangcoding.svg" className="h-20 w-44" />
         <img src="/icons/Google.svg" className="h-20 w-44" />
@@ -10,7 +10,6 @@ const Banner = () => {
         <img src="/icons/Mangcoding.svg" className="h-20 w-44" />
         <img src="/icons/Google.svg" className="h-20 w-44" />
         <img src="/icons/MStore_id.svg" className="h-20 w-44" />
-        <img src="/icons/Mangcoding.svg" className="h-20 w-44" />
       </div>
     </div>
   );
